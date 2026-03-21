2de5b6fa05b9e536886444084d0fe38d865df6b4
Jason Li <z5481421@ad.unsw.edu.au>
2026-03-21 18:06:32 +1100

    SAAS-163: Adding image to login page

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,6 @@
 import { Gelasio, Roboto } from "next/font/google";
 import Form from "next/form";
+import Image from "next/image";
 
 const gelasio = Gelasio({
   subsets: ["latin"],
@@ -23,7 +24,7 @@ export default function Login() {
             Welcome Back
           </h1>
           <Form action="/">
-            <div className="flex flex-col">
+            <div className="flex flex-col pt-10">
               <label
                 htmlFor="email-input"
                 className={`text-[#5c5a5a] ${roboto.className} tracking-widest`}
@@ -37,7 +38,7 @@ export default function Login() {
                 placeholder="name@example.com"
               ></input>
             </div>
-            <div className="flex flex-col">
+            <div className="flex flex-col pt-10">
               <label
                 htmlFor="password-input"
                 className={`text-[#5c5a5a] ${roboto.className} tracking-widest`}
@@ -51,12 +52,29 @@ export default function Login() {
                 placeholder="********"
               ></input>
               <button
-                className={`bg-[#5f5e5e] ${roboto.className} tracking-widest`}
+                className={`bg-[#5f5e5e] ${roboto.className} tracking-widest mt-10`}
               >
                 SIGN IN
               </button>
             </div>
           </Form>
+          <p className={`text-[#5c5a5a] text-center pt-10`}>OR</p>
+          <button
+            className={`text-black tracking-widest border-2 border-black block w-full box-border mt-10`}
+          >
+            CREATE AN ACCOUNT
+          </button>
+          <p className={`italic text-center text-[#5c5a5a] mt-10`}>
+            By continuining, you agree to our Terms of Service
+          </p>
+        </div>
+        <div className="relative w-full h-full overflow-hidden">
+          <Image
+            src="/images/login_image.png"
+            fill={true}
+            className="scale-145 opacity-40"
+            alt="Image of flower and vase, grey-scale"
+          ></Image>
         </div>
       </div>
       <div className="bg-[#faf9f7] flex justify-between">
