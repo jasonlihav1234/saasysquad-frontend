1e7e42efa5b9c3339142ca62af60a6ff3cc985f1
Jason Li <z5481421@ad.unsw.edu.au>
2026-03-21 18:56:08 +1100

    SAAS-163: Added quote and added hover of buttons

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -12,28 +12,32 @@ const roboto = Roboto({
 
 export default function Login() {
   return (
-    <main className="bg-white h-screen grid grid-rows-2">
+    <main className="bg-[#F9F8F6] h-screen grid grid-rows-2">
       <div className="grid grid-cols-3 row-span-2">
         <h3 className={`text-black ${gelasio.className} text-3xl ml-5 pt-5`}>
           The Curated Althaïr
         </h3>
-        <div>
+        <div className="">
           <h1
-            className={`text-black ${gelasio.className} items-center justify-center text-6xl pt-50`}
+            className={`text-black ${gelasio.className} text-center text-7xl pt-50`}
           >
             Welcome Back
           </h1>
-          <Form action="/">
+          <p className={`text-[#5F5E5E] mt-5 ${roboto.className} text-center`}>
+            Luxury is not in the expense, but in the intentional curation of
+            functional beauty.
+          </p>
+          <Form action="/" className="pt-10">
             <div className="flex flex-col pt-10">
               <label
                 htmlFor="email-input"
-                className={`text-[#5c5a5a] ${roboto.className} tracking-widest`}
+                className={`text-[#5c5a5a] ${roboto.className} tracking-widest mb-3`}
               >
                 EMAIL ADDRESS
               </label>
               <input
                 name="EMAIL ADDRESS"
-                className={`bg-[#faf9f7] border-2 border-gray text-[#5c5a5a]`}
+                className={`${roboto.className} focus:outline-none bg-[#E3E2E0] border-b-1 border-[#C5A059] text-[#5c5a5a] -mr-5 z-10 p-6`}
                 id="email-input"
                 placeholder="name@example.com"
               ></input>
@@ -41,29 +45,31 @@ export default function Login() {
             <div className="flex flex-col pt-10">
               <label
                 htmlFor="password-input"
-                className={`text-[#5c5a5a] ${roboto.className} tracking-widest`}
+                className={`text-[#5c5a5a] ${roboto.className} tracking-widest mb-3`}
               >
                 PASSWORD
               </label>
               <input
                 name="PASSWORD"
-                className={`bg-[#faf9f7] border-2 border-gray text-[#5c5a5a]`}
+                className={`${roboto.className} focus:outline-none bg-[#E3E2E0] border-b-1 border-[#C5A059] text-[#5c5a5a] -mr-5 z-10 p-6 mb-5`}
                 id="password-input"
                 placeholder="********"
               ></input>
               <button
-                className={`bg-[#5f5e5e] ${roboto.className} tracking-widest mt-10`}
+                className={`bg-[#474747] hover:bg-[#303030] transition duration-300 ${roboto.className} tracking-widest mt-10 -mr-5 z-10 p-6 text-white font-bold`}
               >
                 SIGN IN
               </button>
+
+              <p className={`text-[#5c5a5a] text-center pt-10`}>OR</p>
+              <button
+                type="button"
+                className={`relative text-black bg-[#f8f8f8] hover:bg-white transition duration-300 font-bold tracking-widest border-1 border-[#C5A059] block mt-10 -mr-5 p-6 z-10`}
+              >
+                CREATE AN ACCOUNT
+              </button>
             </div>
           </Form>
-          <p className={`text-[#5c5a5a] text-center pt-10`}>OR</p>
-          <button
-            className={`text-black tracking-widest border-2 border-black block w-full box-border mt-10`}
-          >
-            CREATE AN ACCOUNT
-          </button>
           <p className={`italic text-center text-[#5c5a5a] mt-10`}>
             By continuining, you agree to our Terms of Service
           </p>
@@ -72,12 +78,12 @@ export default function Login() {
           <Image
             src="/images/login_image.png"
             fill={true}
-            className="scale-145 opacity-40"
+            className="scale-145 opacity-30"
             alt="Image of flower and vase, grey-scale"
           ></Image>
         </div>
       </div>
-      <div className="bg-[#faf9f7] flex justify-between">
+      <div className="bg-[#F1F1EF] flex justify-between">
         <p
           className={`text-[#5c5a5a] ${roboto.className} pl-15 pb-15 pt-15 tracking-wider text-sm`}
         >
