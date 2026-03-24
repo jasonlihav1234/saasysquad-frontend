8064612987123f0983695c225aafa2500b3c851c
Jason Li <z5481421@ad.unsw.edu.au>
2026-03-24 20:40:27 +1100

    SAAS-184: Added function for calling reset password api

--- src/app/reset-password/page.tsx
+++ src/app/reset-password/page.tsx
@@ -3,6 +3,7 @@
 import { Gelasio, Roboto } from "next/font/google";
 import { useRef, useState } from "react";
 import Link from "next/link";
+import { useSearchParams, useRouter } from "next/navigation";
 
 const gelasio = Gelasio({
   subsets: ["latin"],
@@ -17,13 +18,19 @@ const roboto = Roboto({
 export default function ResetPasswordPage() {
   const [isSubmitting, setIsSubmitting] = useState(false);
   const isSubmittingRef = useRef<boolean>(false);
+  const searchParams = useSearchParams();
+  const router = useRouter();
 
   const handleSubmit = async (e: any) => {
     e.preventDefault();
     const formData = new FormData(e.currentTarget);
+    const token = searchParams.get("token");
 
     if (isSubmittingRef.current) {
       return;
+    } else if (!token) {
+      alert("Token not found");
+      setTimeout(() => router.push("/login"), 3000);
     }
 
     isSubmittingRef.current = true;
@@ -39,19 +46,29 @@ export default function ResetPasswordPage() {
     }
 
     try {
-      const response = fetch(
+      const response = await fetch(
         "https://sassysquad-backend.vercel.app/auth/reset-password",
         {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
           },
-          body: JSON.stringify({}),
+          body: JSON.stringify({ token }),
         },
       );
+
+      if (response.status === 200) {
+        alert("Password successfully changed");
+        setTimeout(() => router.push("/login"), 2000);
+      } else {
+        const data = await response.json();
+        alert(`Issue with resetting password, try again: ${data}`);
+      }
     } catch (error) {
       alert(error);
     } finally {
+      setIsSubmitting(false);
+      isSubmittingRef.current = false;
     }
   };
 
