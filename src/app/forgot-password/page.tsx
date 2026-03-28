843ae2dc045616a9818fc85001a0b20ef03db6aa
Jason Li <166970548+jasonlihav1234@users.noreply.github.com>
2026-03-28 13:03:39 +1100

    SAAS-183: Reset password page (#5)
    * SAAS-182: Squashing previous commits
    
    * SAAS-182: Added blur for loading image
    
    * SAAS-182: Adding calling api for registering in frontend
    
    * SAAS-182: Added handling submission for registering
    
    * SAAS-182: Creating basic reset password page
    
    * SAAS-183: Added styling to page
    
    * SAAS-183: Adding email api to frontend
    
    * SAAS-183: Change url to prod
    
    ---------
    
    Co-authored-by: Jason Li <z5481421@ad.unsw.edu.au>

--- src/app/forgot-password/page.tsx
+++ src/app/forgot-password/page.tsx
@@ -17,49 +17,124 @@ const roboto = Roboto({
 });
 
 export default function ForgotPasswordPage() {
+  const [isSubmitting, setIsSubmitting] = useState(false);
+  const isSubmittingRef = useRef<boolean>(false);
+  const router = useRouter();
+
+  const handleSubmit = async (e: any) => {
+    e.preventDefault();
+    const formData = new FormData(e.currentTarget);
+
+    if (isSubmittingRef.current) {
+      return;
+    }
+
+    isSubmittingRef.current = true;
+    setIsSubmitting(true);
+
+    try {
+      const response = await fetch(
+        "https://sassysquad-backend.vercel.app/auth/forgot-password",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({
+            email: formData.get("email"),
+          }),
+        },
+      );
+
+      if (response.status === 200) {
+        alert("Email sent");
+      } else {
+        alert("Email failed to send");
+      }
+    } catch (error) {
+      console.log(error);
+      alert("Failure to send email");
+    } finally {
+      setIsSubmitting(false);
+      isSubmittingRef.current = false;
+    }
+  };
+
   return (
-    <main className="grid grid-rows-3 justify-center items-center bg-[#F9F8F6] min-h-screen w-full">
-      <h1 className={`${gelasio.className} text-black text-3xl text-center`}>
-        The Curated Althair
+    <main className="grid grid-rows-[auto_1fr_auto] bg-[#F9F8F6] min-h-screen w-full">
+      <h1
+        className={`${gelasio.className} text-black text-3xl text-center pt-10`}
+      >
+        The Curated Althaïr
       </h1>
-      <div className="flex h-full flex-col justify-center">
-        <h1 className={`${gelasio.className} text-black text-4xl text-center`}>
+
+      <div className="flex h-full flex-col items-center pt-50 px-6 w-full">
+        <h1
+          className={`${gelasio.className} text-black text-5xl md:text-6xl text-center`}
+        >
           Reset Password
         </h1>
-        <p className={`text-center ${roboto.className} text-black`}>
-          Enter you email address and we'll send you a link to reset your
+
+        <p
+          className={`text-center ${roboto.className} text-[#787777] pt-5 text-lg max-w-sm`}
+        >
+          Enter your email address and we'll send you a link to reset your
           password.
         </p>
 
-        <form>
+        <form onSubmit={handleSubmit} className="pt-16 w-full max-w-md">
           <div className="flex flex-col">
             <label
               htmlFor="email-reset"
-              className={`text-[#5c5a5a] ${roboto.className} tracking-widest mb-3 cursor-text`}
+              className={`text-[#5c5a5a] ${roboto.className} tracking-widest pb-3 cursor-text`}
             >
               EMAIL ADDRESS
             </label>
             <input
               type="email"
+              name="email"
               placeholder="name@example.com"
-              className={`${roboto.className} focus:outline-none bg-[#E3E2E0] border-b-1 border-[#C5A059] text-[#5c5a5a] p-6 disbled:opacity-80 disabled:cursor-not-allowed`}
+              className={`${roboto.className} w-full focus:outline-none bg-[#E3E2E0] border-b-2 border-[#C5A059] text-[#5c5a5a] p-5 disabled:opacity-80 disabled:cursor-not-allowed`}
+              disabled={isSubmitting}
               required
-            ></input>
+            />
             <button
               type="submit"
-              className={`cursor-pointer bg-[#474747] hover:bg-[#303030] transition duration-300 ${roboto.className} tracking-widest mt-10 text-white font-bold disabled:opacity-80 p-6`}
+              disabled={isSubmitting}
+              className={`cursor-pointer w-full bg-[#474747] hover:bg-[#303030] transition duration-300 ${roboto.className} tracking-widest mt-12 text-white font-bold disabled:opacity-80 p-5 disbled:opacity-80 disabled:cursor-not-allowed`}
             >
               SEND RESET LINK →
             </button>
           </div>
         </form>
+
         <p
-          className={`underline text-[#5c5a5a] ${roboto.className} text-center cursor-pointer`}
+          className={`tracking-widest underline decoration-[#E9C176] underline-offset-10 text-[#5c5a5a] ${roboto.className} text-center cursor-pointer pt-7`}
         >
-          RETURN TO LOGIN
+          <Link href="/login">RETURN TO LOGIN</Link>
         </p>
+        <hr className="border border-[#E3E2E0] w-32 mt-50"></hr>
       </div>
-      <div></div>
+
+      <div className="bg-[#F1F1EF] flex justify-between">
+        <p
+          className={`text-[#5c5a5a] ${roboto.className} pl-15 pb-15 pt-15 tracking-wider text-sm`}
+        >
+          © 2026 THE CURATED ALTHAïR. ALL RIGHTS RESERVED.
+        </p>
+        <div className="flex flex-row pb-15 pt-15">
+          <p
+            className={`cursor-pointer text-[#5c5a5a] ${roboto.className} tracking-wider text-sm pr-15 hover:underline`}
+          >
+            PRIVACY POLICY
+          </p>
+          <p
+            className={`cursor-pointer text-[#5c5a5a] ${roboto.className} tracking-wider text-sm pr-15 hover:underline`}
+          >
+            SUPPORT
+          </p>
+        </div>
+      </div>
     </main>
   );
 }
