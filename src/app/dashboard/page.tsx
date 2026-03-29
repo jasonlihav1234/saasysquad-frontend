364be5df70916a587032b395e8743f28b436e187
Jason Li <z5481421@ad.unsw.edu.au>
2026-03-29 15:30:03 +1100

    SAAS-193: Adding setup code for dashboard

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,3 +1,31 @@
+"use client";
+import Footer from "@/components/universal/Footer";
+import { Roboto, Gelasio } from "next/font/google";
+import { useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+
+// probably should make this user/dashboard
+
+const roboto = Roboto({
+  subsets: ["latin"],
+  style: ["normal", "italic"],
+});
+
+const gelasio = Gelasio({
+  subsets: ["latin"],
+  style: ["normal", "italic"],
+});
+
 export default function Dashboard() {
-  return <h1>Dashboard Page</h1>;
+  return (
+    <>
+      <nav></nav>
+      <main className="bg-[#F9F8F6] min-h-screen w-full">
+        <div>
+          <p className="text-black">Test</p>
+        </div>
+        <Footer />
+      </main>
+    </>
+  );
 }
