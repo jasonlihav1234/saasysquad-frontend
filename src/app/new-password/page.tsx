7a6ce6318d21c2a2e4bb5bdc1498014cf6b35d15
Uday Saihgal <z5507339@ad.unsw.edu.au>
2026-04-02 21:25:38 +1100

    Finished new password static page

--- src/app/new-password/page.tsx
+++ src/app/new-password/page.tsx
@@ -1,9 +1,14 @@
 "use client";
 
-import { Roboto } from "next/font/google";
+import { Gelasio, Roboto } from "next/font/google";
 import Link from "next/link";
 import Footer from "@/components/universal/Footer";
 import PasswordPageBrandingHeader from "@/components/universal/PasswordPageBrandingHeader";
+
+const gelasio = Gelasio({
+  subsets: ["latin"],
+  style: ["normal", "italic"],
+});
 
 const roboto = Roboto({
   subsets: ["latin"],
@@ -25,19 +30,19 @@ export default function CreateNewPasswordPage() {
       <PasswordPageBrandingHeader />
 
       <div
-        className={`flex bg-[${color.colorWhite}] border border-solid border-black items-center justify-center flex-col`}
+        className={`flex bg-[${color.colorWhite}] items-center justify-center flex-col`}
       >
-        <h1 className={`text-black text-5xl md:text-6xl text-center`}>
+        <h1 className={`text-black text-5xl md:text-6xl text-center ${gelasio.className}`}>
           Create New <br></br> Password
         </h1>
 
         <p
-          className={`text-center text-[${color.colorGreyLight}] pt-5 text-lg max-w-sm`}
+          className={`text-center text-[${color.colorGreyLight}] pt-5 text-lg max-w-sm ${roboto.className}`}
         >
           Please enter your new credentials below.
         </p>
         <div>
-          <form className="pt-16 w-full max-w-md border border-black border-solid">
+          <form className="pt-16 w-full max-w-md">
             <label
               htmlFor="new-password"
               className={`text-[#5c5a5a] ${roboto.className} tracking-widest pb-3 cursor-text`}
@@ -48,7 +53,7 @@ export default function CreateNewPasswordPage() {
               type="password"
               name="new-password"
               placeholder="**************"
-              className={`${roboto.className} w-full focus:outline-none bg-[#E3E2E0] border-b-2 border-[#C5A059] text-[#5c5a5a] p-5 disabled:opacity-80 disabled:cursor-not-allowed`}
+              className={`${roboto.className} w-full focus:outline-none bg-[#E3E2E0] border-b-2 border-[#C5A059] text-[#5c5a5a] p-5 mb-8 disabled:opacity-80 disabled:cursor-not-allowed`}
               required
             />
             <label
