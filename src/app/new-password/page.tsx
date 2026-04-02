ff3b59187e8bf600bd58aa42ad182605d4b2252b
Uday Saihgal <z5507339@ad.unsw.edu.au>
2026-04-02 15:49:51 +1100

    Added title and footer

--- src/app/new-password/page.tsx
+++ src/app/new-password/page.tsx
@@ -5,6 +5,7 @@ import Image from "next/image";
 import Link from "next/link";
 import { useState, useRef } from "react";
 import { useRouter } from "next/navigation";
+import Footer from "@/components/universal/Footer";
 
 const gelasio = Gelasio({
   subsets: ["latin"],
@@ -16,6 +17,21 @@ const roboto = Roboto({
   style: ["normal", "italic"],
 });
 
+const colorWhite = "#F9F8F6";
+
 export default function CreateNewPasswordPage () {
-    return (<></>);
+    return (
+    <main className={`bg-[${colorWhite}] grid grid-rows-[auto_1fr_auto] min-h-screen w-full`}>
+        <h1
+        className={`${gelasio.className} text-black text-3xl text-center pt-10`}
+      >
+        The Curated Althaïr
+      </h1>
+
+      <div className={`flex bg-[${colorWhite}] border border-solid border-black`}>
+
+      </div>
+
+      <Footer />
+    </main>);
 }
