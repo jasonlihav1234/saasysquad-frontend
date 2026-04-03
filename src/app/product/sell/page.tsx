4b3edaa431f2920c81cbd784704ef6b5d916dd94
Jason Li <z5481421@ad.unsw.edu.au>
2026-04-03 16:13:00 +1100

    SAAS-202: Formatting jsx

--- src/app/product/sell/page.tsx
+++ src/app/product/sell/page.tsx
@@ -22,56 +22,69 @@ const gelasio = Gelasio({
 
 export default function SellProducePage() {
   return (
-    <header className="fixed top-0 w-full z-50 glass-nav">
-      <div className="flex gap-8 justify-between items-center px-12 py-6 w-full max-w-[1920px] mx-auto">
-        <span
-          className={`text-2xl ${gelasio.className} tracking-tighter text-[#1A1C1B] dark:text-[#FAF9F7]`}
-        >
-          The Curated Althaïr
-        </span>
-        <div className="hidden md:flex gap-6 items-center">
-          <Link
-            href="/dashboard"
-            className={`text-[#5f5e5e] dark:text-[#a7a5a5] hover:text-[#1a1c1b] hover:text-[#775a19] transition-colors duration-300`}
+    <>
+      <header className="fixed top-0 w-full z-50 glass-nav">
+        <div className="flex gap-8 justify-between items-center px-12 py-6 w-full max-w-[1920px] mx-auto">
+          <span
+            className={`text-2xl ${gelasio.className} tracking-tighter text-[#1A1C1B] dark:text-[#FAF9F7]`}
           >
-            Catalog
-          </Link>
-          <Link
-            href="/purchases"
-            className={`text-[#5f5e5e] dark:text-[#a7a5a5] hover:text-[#1a1c1b] hover:text-[#775a19] transition-colors duration-300`}
-          >
-            Purchases
-          </Link>
-          <Link
-            href="/sales"
-            className={`text-[#5f5e5e] dark:text-[#a7a5a5] hover:text-[#1a1c1b] hover:text-[#775a19] transition-colors duration-300`}
-          >
-            Sales
-          </Link>
-          <Link
-            href="/messages"
-            className={`text-[#5f5e5e] dark:text-[#a7a5a5] hover:text-[#1a1c1b] hover:text-[#775a19] transition-colors duration-300`}
-          >
-            Messages
-          </Link>
-          <Link
-            href="/product/sell"
-            className={`text-[#775A19] dark:text-[#C5A059] border-b border-[#775A19]`}
-          >
-            Sell Product
-          </Link>
+            The Curated Althaïr
+          </span>
+          <div className="hidden md:flex gap-6 items-center">
+            <Link
+              href="/dashboard"
+              className={`text-[#5f5e5e] dark:text-[#a7a5a5] hover:text-[#1a1c1b] hover:text-[#775a19] transition-colors duration-300`}
+            >
+              Catalog
+            </Link>
+            <Link
+              href="/purchases"
+              className={`text-[#5f5e5e] dark:text-[#a7a5a5] hover:text-[#1a1c1b] hover:text-[#775a19] transition-colors duration-300`}
+            >
+              Purchases
+            </Link>
+            <Link
+              href="/sales"
+              className={`text-[#5f5e5e] dark:text-[#a7a5a5] hover:text-[#1a1c1b] hover:text-[#775a19] transition-colors duration-300`}
+            >
+              Sales
+            </Link>
+            <Link
+              href="/messages"
+              className={`text-[#5f5e5e] dark:text-[#a7a5a5] hover:text-[#1a1c1b] hover:text-[#775a19] transition-colors duration-300`}
+            >
+              Messages
+            </Link>
+            <Link
+              href="/product/sell"
+              className={`text-[#775A19] dark:text-[#C5A059] border-b border-[#775A19]`}
+            >
+              Sell Product
+            </Link>
+          </div>
+          <div className="w-8 h-8 bg-surface-container-highest flex items-center justify-center overflow-hidden">
+            <Link href="/me">
+              <Image
+                src="https://lh3.googleusercontent.com/aida-public/AB6AXuAIEWWIsS24oEuhS288WfCZjz-DYqXxsIG0aCNeFSv78p1rnf6XwcNzKSw-Xn1_AUFH_ESsayZqp-A6g9FAOCencuC1ka2p90hh06vwU4RCpA5Hwuk70p6PViQLxszYYVWfaLRm4VcP-tFyWJY2Zgqmwlg37Yt-iN7qKnSfl812uX1V6D9gAzX43IGcr63yiDKlxJjky5qS3cDTR63mrstO31kxFyupT6m7F2_peMXjtNvbrgTXD5doEoG3vBr0gESyhoIGCZvtgLi7"
+                alt="User Profile"
+                width={32}
+                height={32}
+              ></Image>
+            </Link>
+          </div>
         </div>
-        <div className="w-8 h-8 bg-surface-container-highest flex items-center justify-center overflow-hidden">
-          <Link href="/me">
-            <Image
-              src="https://lh3.googleusercontent.com/aida-public/AB6AXuAIEWWIsS24oEuhS288WfCZjz-DYqXxsIG0aCNeFSv78p1rnf6XwcNzKSw-Xn1_AUFH_ESsayZqp-A6g9FAOCencuC1ka2p90hh06vwU4RCpA5Hwuk70p6PViQLxszYYVWfaLRm4VcP-tFyWJY2Zgqmwlg37Yt-iN7qKnSfl812uX1V6D9gAzX43IGcr63yiDKlxJjky5qS3cDTR63mrstO31kxFyupT6m7F2_peMXjtNvbrgTXD5doEoG3vBr0gESyhoIGCZvtgLi7"
-              alt="User Profile"
-              width={32}
-              height={32}
-            ></Image>
-          </Link>
+      </header>
+      <main
+        className={`pt-32 pb-24 px-6 md:px-12 flex-groew max-w-[1920px] mx-auto w-full`}
+      >
+        <div className="grid grid-cols1 lg:grid-cols-12 gap-16">
+          <div className="lg:col-span-7 space-y-16">
+            <header>
+              <p className={`${gelasio.className} text-5xl font-black tracking-tight leading-tight text-[#1a1c1b]`}>List a New Item</p>
+            </header>
+          </div>
         </div>
-      </div>
-    </header>
+      </main>
+    </>
   );
 }
