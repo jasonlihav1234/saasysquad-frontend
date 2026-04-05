7d0cecc7f028ab1fc1b0c77bd9b41a3804150c15
Jason Li <z5481421@ad.unsw.edu.au>
2026-04-05 18:50:01 +1000

    SAAS-207: Updated fields

--- src/app/dashboard/pageData.js
+++ src/app/dashboard/pageData.js
@@ -4,7 +4,7 @@ export const testItem1 = {
   price: 420,
   image_url: "https://lh3.googleusercontent.com/aida-public/AB6AXuB7Oh2iFlSedCOJJ3qdZSgrAzh-bPA9AEHxG9QGLhNzjHs4B7XrVjolE6ZnACqryENlH5-Q5Q4GLC-6kJE11ZjCnNBv85iWpH9SRi_NJvsj39H9QSfSd9fRzYGQVFUtzaCpP-BcjNDi55Yy9_G74LyiXIDrumpmgFJAloEDU30u5OAX993pAlUmX12ciNQ_57zT2V-tecxFk-tVbRoaVUEMYUZA6k7ZkaKopoGRxsELyzwvZ9kc7MCCzLQSOOQjwnd0aWRwf3sZa_Ow",
   maker: "John Smith",
-  date: "2025-10-15",
+  last_updated: "2025-10-15",
   items_sold: 142
 };
 
@@ -14,7 +14,7 @@ export const testItem2 = {
   price: 420,
   image_url: "https://lh3.googleusercontent.com/aida-public/AB6AXuB7Oh2iFlSedCOJJ3qdZSgrAzh-bPA9AEHxG9QGLhNzjHs4B7XrVjolE6ZnACqryENlH5-Q5Q4GLC-6kJE11ZjCnNBv85iWpH9SRi_NJvsj39H9QSfSd9fRzYGQVFUtzaCpP-BcjNDi55Yy9_G74LyiXIDrumpmgFJAloEDU30u5OAX993pAlUmX12ciNQ_57zT2V-tecxFk-tVbRoaVUEMYUZA6k7ZkaKopoGRxsELyzwvZ9kc7MCCzLQSOOQjwnd0aWRwf3sZa_Ow",
   maker: "John Smith",
-  date: "2023-10-15",
+  last_updated: "2023-10-15",
   items_sold: 142
 };
 
@@ -24,7 +24,7 @@ export const testItem3 = {
   price: 420,
   image_url: "https://lh3.googleusercontent.com/aida-public/AB6AXuB7Oh2iFlSedCOJJ3qdZSgrAzh-bPA9AEHxG9QGLhNzjHs4B7XrVjolE6ZnACqryENlH5-Q5Q4GLC-6kJE11ZjCnNBv85iWpH9SRi_NJvsj39H9QSfSd9fRzYGQVFUtzaCpP-BcjNDi55Yy9_G74LyiXIDrumpmgFJAloEDU30u5OAX993pAlUmX12ciNQ_57zT2V-tecxFk-tVbRoaVUEMYUZA6k7ZkaKopoGRxsELyzwvZ9kc7MCCzLQSOOQjwnd0aWRwf3sZa_Ow",
   maker: "John Smith",
-  date: "2020-10-15",
+  last_updated: "2020-10-15",
   items_sold: 142
 };
 
@@ -34,7 +34,7 @@ export const testItem4 = {
   price: 420,
   image_url: "https://lh3.googleusercontent.com/aida-public/AB6AXuB7Oh2iFlSedCOJJ3qdZSgrAzh-bPA9AEHxG9QGLhNzjHs4B7XrVjolE6ZnACqryENlH5-Q5Q4GLC-6kJE11ZjCnNBv85iWpH9SRi_NJvsj39H9QSfSd9fRzYGQVFUtzaCpP-BcjNDi55Yy9_G74LyiXIDrumpmgFJAloEDU30u5OAX993pAlUmX12ciNQ_57zT2V-tecxFk-tVbRoaVUEMYUZA6k7ZkaKopoGRxsELyzwvZ9kc7MCCzLQSOOQjwnd0aWRwf3sZa_Ow",
   maker: "John Smith",
-  date: "2024-10-15",
+  last_updated: "2024-10-15",
   items_sold: 142
 };
 
