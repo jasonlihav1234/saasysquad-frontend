6c8a42482fa6f44dc1341017977b16b49d15f461
Jason Li <z5481421@ad.unsw.edu.au>
2026-04-05 13:39:57 +1000

    SAAS-208: Added fetching checkout session use effect

--- src/app/return/page.tsx
+++ src/app/return/page.tsx
@@ -6,6 +6,38 @@ import Link from "next/link";
 
 
 export default function ReturnPage() {
+  const [status, setStatus] = useState<string | null>(null);
+  const [customerEmail, setCustomerEmail] = useState("");
+  const searchParams = useSearchParams();
+  const router = useRouter();
+  const sessionId = searchParams.get("session_id");
+
+  useEffect(() => {
+    if (!sessionId) {
+      return;
+    }
+    
+    const fetchSessionStatus = async () => {
+      try {
+        const response = await fetch(`https://sassysquad-backend.vercel.app/checkout-session-status/${sessionId}`);
+        const body = await response.json();
+
+        setStatus(body.status);
+        setCustomerEmail(body.customer_email);
+      } catch (error) {
+        console.log(error);
+      }
+    };
+
+    fetchSessionStatus();
+  }, [sessionId]);
+
+  // payment failed, send to checkout
+  if (status === "open") {
+    router.push("/checkout");
+    return null;
+  }
+
   return (
 
   );
