1cbd595deb592cbf277ad011d004642bf32bbe23
Jason Li <z5481421@ad.unsw.edu.au>
2026-04-05 13:33:09 +1000

    SAAS-208: Making return page for payments

---
+++ src/app/return/page.tsx
@@ -0,0 +1,12 @@
+"use client"
+
+import { useEffect, useState, Suspense } from "react";
+import { useSearchParams, useRouter } from "next/navigation";
+import Link from "next/link";
+
+
+export default function ReturnPage() {
+  return (
+
+  );
+}
