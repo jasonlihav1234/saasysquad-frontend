2a045ce113504ac9cdfe4d311538ef2a8af661d0
Jason Li <z5481421@ad.unsw.edu.au>
2026-04-16 23:53:36 +1000

    SAAS-229: Adding check status useeffect

--- src/app/subscribe/folder/page.tsx
+++ src/app/subscribe/folder/page.tsx
@@ -1,4 +1,4 @@
-"use client"
+"use client";
 
 import { Gelasio, Roboto } from "next/font/google";
 import { useEffect, useState } from "react";
@@ -8,9 +8,60 @@ import "material-symbols";
 
 const roboto = Roboto({ subsets: ["latin"], style: ["normal", "italic"] });
 const gelasio = Gelasio({ subsets: ["latin"], style: ["normal", "italic"] });
- 
+
 type Status = "loading" | "success" | "processing" | "failed";
 
 export default function SubscriptionReturnPage() {
-  
+  const router = useRouter();
+  const searchParams = useSearchParams();
+  const sessionId = searchParams.get("session_id");
+
+  const [status, setStatus] = useState<Status>("loading");
+  const [email, setEmail] = useState<string | null>(null);
+  const [visible, setVisible] = useState<boolean>(false);
+
+  useEffect(() => {
+    const t = setTimeout(() => setVisible(true), 100);
+
+    return () => clearTimeout(t);
+  }, []);
+
+  useEffect(() => {
+    if (!sessionId) {
+      setStatus("failed");
+      return;
+    }
+
+    const checkStatus = async () => {
+      try {
+        const res = await fetch(
+          `https://sassysquad-backend.vercel.app/checkout-session-status?session_id=${sessionId}`,
+          { method: "GET" },
+        );
+
+        const data = await res.json();
+
+        if (!res.ok) {
+          setStatus("failed");
+          return;
+        }
+
+        setEmail(data.customer_email);
+
+        if (data.status === "complete") {
+          setStatus("success");
+        } else if (data.status === "open") {
+          setStatus("processing");
+          setTimeout(checkStatus, 1500);
+        } else {
+          setStatus("failed");
+        }
+      } catch (error) {
+        console.log(error);
+        setStatus("failed");
+      }
+    };
+
+    checkStatus();
+  }, [sessionId]);
 }
