bdb08e65df012e7ffdfddd681498674240ece200
Jason Li <z5481421@ad.unsw.edu.au>
2026-04-19 16:12:19 +1000

    SAAS-237: Creating auth fetch for refreshing tokens

--- lib/api.ts
+++ lib/api.ts
@@ -1,4 +1,3 @@
-
 let isRefreshing = false;
 let refreshPromise: Promise<string> | null = null;
 
@@ -9,21 +8,24 @@ async function getNewToken(): Promise<string> {
 
   isRefreshing = false;
 
-  refreshPromise = (async() => {
+  refreshPromise = (async () => {
     try {
       const refreshToken = localStorage.getItem("refreshToken");
       if (!refreshToken) throw new Error("No refresh token");
 
-      const res = await fetch("https://sassysquad-backend.vercel.app/auth/refresh", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json"
+      const res = await fetch(
+        "https://sassysquad-backend.vercel.app/auth/refresh",
+        {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+          },
+          body: JSON.stringify({ refreshToken }),
         },
-        body: JSON.stringify({ refreshToken }),
-      });
+      );
 
       if (!res.ok) {
-        throw new Error("Refresh failed")
+        throw new Error("Refresh failed");
       }
 
       const data = await res.json();
@@ -42,3 +44,33 @@ async function getNewToken(): Promise<string> {
 
   return refreshPromise;
 }
+
+export const authFetch = async (url: string, options: RequestInit = {}) => {
+  let token = localStorage.getItem("accessToken");
+
+  const getHeaders = (t: string | null) => ({
+    ...options.headers,
+    Authorization: `Bearer ${t}`,
+    "Content-Type": "application/json",
+  });
+
+  let response = await fetch(url, { ...options, headers: getHeaders(token) });
+
+  if (response.status === 401) {
+    try {
+      const newToken = await getNewToken();
+
+      response = await fetch(url, {
+        ...options,
+        headers: getHeaders(newToken),
+      });
+    } catch (error: any) {
+      console.log(error);
+      localStorage.clear();
+      window.location.href = "/login";
+      return Promise.reject("Session Expired");
+    }
+  }
+
+  return response;
+};
